fc9627c347110459d6fba5d24c1a9e5644f5b970
test <tjrwowkd@gmail.com>
2026-06-08 18:46:18 +0900

    20260608_2

--- react-front/src/index.js
+++ react-front/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // 👈 이 줄이 누락되어 있다면 변수가 프로젝트에 주입되지 않습니다!
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
